8965c1c1b1a7617b2e0c02eea1e07576c8591bdb
Fernando Butzke <nandoproject209@gmail.com>
2021-01-25 20:59:50 -0300

    the first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata';
+import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+
+export default routes;
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import routes from './routes/index';
+
+import './database';
+
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World!' });
+});
+
+app.listen(3333, () => {
+    console.log('✔ Server started on port 3333!');
+});

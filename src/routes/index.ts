1c9b4ca607f00a96fba25dd4c375c1ac72d1c460
Fernando Butzke <nandoproject209@gmail.com>
2021-01-28 22:38:54 -0300

    Adding CreateUserService // users routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;

8b915d46d9cef051dfffe995c0200edec3cc9b61
Fernando Butzke <nandoproject209@gmail.com>
2021-03-06 20:25:17 -0300

    up

--- src/server.ts
+++ src/server.ts
@@ -11,18 +11,18 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, _:NextFunction) => {
-    if(err instanceof AppError) {
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
         return response.status(err.statusCode).json({
-            status: "error",
+            status: 'error',
             message: err.message,
         });
     }
 
     return response.status(500).json({
-        status: "error",
-        message: "Internal server error",
-    })
+        status: 'error',
+        message: 'Internal server error',
+    });
 });
 
 app.get('/', (request, response) => {
